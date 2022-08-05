9637c36c120823cb6be5644b1ef167f019048179
YourBoyConnor <connor.pymm@gmail.com>
2022-08-05 13:29:51 -0400

    Added Goodman and Finger

---
+++ Commands/goodman.js
@@ -0,0 +1,47 @@
+const { PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus } = require('@discordjs/voice');
+const { join } = require('node:path');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('goodman')
+    .setDescription('BWAOWAOOOOOOO BOOODOLODLOLOLODOOOOOOOOOOOOOO DO DO DO DOOOOUOUO'),
+  async execute (client, interaction) {
+
+    const player = createAudioPlayer();
+
+    const voiceChannel = interaction.member.voice.channel;
+
+    if (!voiceChannel) return interaction.reply("You need to be in a VC to use this command!");
+
+    if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.Connect)) return interaction.reply("I am not allowed to connect to VC!");
+
+    if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.Speak)) return interaction.reply("I am not allowed to speak in VC!");
+
+    const resource = createAudioResource(join(__dirname, 'Audio/goodman.mp3'));
+
+    player.on(AudioPlayerStatus.Playing, () => {
+      console.log('The Audio Player is now playing')
+    });
+
+    player.on('error', err => {
+      console.log('Error: ${error.message}');
+    });
+
+    const connection = joinVoiceChannel({
+       channelId: interaction.member.voice.channel.id,
+       guildId: interaction.guild.id,
+       adapterCreator: interaction.guild.voiceAdapterCreator,
+    });
+
+    const subscription = connection.subscribe(player);
+    player.play(resource);
+
+    player.on(AudioPlayerStatus.Idle, () => {
+    	connection.destroy();
+    });
+
+    return interaction.reply('**Saul Goodman**')
+  },
+};
--- deploy-commands.js
+++ deploy-commands.js
@@ -7,9 +7,20 @@ const mySecret = process.env['BOT_TOKEN'];
 const deploySlashCommands = async () => {
    const rest = new REST({ version: '9' }).setToken(mySecret);
    const applicationId = '1003138443506884708'
-   const command = require('./trinkets');
 
-   commands.push(command.data.toJSON());
+   const fs = require('fs');
+   const testFolder = 'Commands';
+
+   fs.readdir(testFolder, (err, files) => {
+     files.forEach(file => {
+       if (!file.toString().endsWith('.js')) return;
+
+       let fileName = file.toString().substring(0, file.toString().length - 3);
+       let filePath = './Commands/' + fileName;
+       let command = require(filePath);
+       commands.push(command.data.toJSON());
+     });
+   });
 
    rest.put(Routes.applicationCommands(applicationId), { body: commands })
    .then(() => console.log('Successfully registered application commands.'))
